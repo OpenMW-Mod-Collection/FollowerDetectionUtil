3227e03d639ac4d56af20c48ca9fee0ac203515c
SosnoviyBor <wormer007@gmail.com>
2026-01-13 20:29:24 +0200

    1.0.1 release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Follower Detection Util (OpenMW)
 
+## 1.0.1
+
+### Fixes
+
+- Fixed error while parsing AI packages with no target
+
 ## 1.0
 
 Initial release
--- scripts/FollowerDetectionUtil/logic/ai.lua
+++ scripts/FollowerDetectionUtil/logic/ai.lua
@@ -3,7 +3,7 @@ local I = require('openmw.interfaces')
 function GetLeader()
     local leader
     I.AI.forEachPackage(function(pkg)
-        if (pkg.type == "Follow" or pkg.type == "Escort") and pkg.target:isValid() then
+        if (pkg.type == "Follow" or pkg.type == "Escort") and pkg.target and pkg.target:isValid() then
             leader = pkg.target
             return
         end
